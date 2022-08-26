3f8826558a57198ded335ad80c28aa3a66b587d7
skelmis <ethan@koldfusion.xyz>
2022-08-26 16:21:45 +1200

    chore: up version

--- aegir/__init__.py
+++ aegir/__init__.py
@@ -6,10 +6,10 @@ from .parsed_data import ParsedData
 from .source_file import SourceFile
 from aegir.aegir import Aegir
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=0, minor=1, micro=1, releaselevel="alpha", serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=2, releaselevel="alpha", serial=0)
 
 __all__ = ("Aegir", "FormatError", "ParsedData", "SourceFile")
